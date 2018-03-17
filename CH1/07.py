74d83b04acec8bc0f8681800a4abb27a38006028
annkara <howtobecomeahacker@gmail.com>
2018-03-17 13:49:17 +0900

    引数を文字列へキャストしていたが、不要のためコメントアウト。 Templateクラスを利用しても、同じ処理は可能。 Templateクラスの用途はXMLやHTMLなどのファイル出力に定形の フォーマットで出力するときに有用かもしれない。

--- CH1/07.py
+++ CH1/07.py
@@ -4,11 +4,10 @@ def str_template(x, y, z):
     '''
     x時のyはzというテンプレートに当てはめた文字列を返す。
     '''
-
-    x_, y_, z_ = str(x), str(y), str(z)
+    # 文字列へのキャストは不要
+    # x_, y_, z_ = str(x), str(y), str(z)
     template = '{}時の{}は{}'
-
-    return template.format(x_, y_, z_)
+    return template.format(x, y, z)
 
 if __name__ == '__main__':
     print(str_template(x=12, y='気温', z=22.4))
