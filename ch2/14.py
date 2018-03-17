9dd7c0f4212469d75edf8c4fac1def8ad147aac0
annkara <howtobecomeahacker@gmail.com>
2018-03-17 18:23:55 +0900

    ファイルオブジェクトから行を読み出す際に、 enumerate関数を利用すれば、コードの短縮に繋がる。

---
+++ ch2/14.py
@@ -0,0 +1,26 @@
+# 14. 先頭からN行を出力
+'''コマンド
+    - head -10 hightemp.txt
+'''
+
+import sys
+
+# 引数のチェック
+if len(sys.argv) > 2:
+    print('Usage : python 14.py <Integer>')
+    sys.exit()
+
+try:
+    row_number = int(sys.argv[1])
+except ValueError as e:
+    print('整数に変換できません。')
+    sys.exit()
+
+text = 'hightemp.txt'
+with open(text, 'rt') as f:
+    count = 0
+    for row in f:
+        if count == row_number:
+            break
+        print(row, end='')
+        count+=1
